5b1cca932f508a3f777ce4fe4eb4696f9f888ef3
Gildeir <gildeirlopes@gmail.com>
2021-10-22 03:27:01 -0300

    criando arquivo blogPostService e refatorando a funçao que cria um blogPost

--- src/controllers/blogPostController.js
+++ src/controllers/blogPostController.js
@@ -3,16 +3,10 @@
   const secret = 'super-senha';
 
  const { BlogPost, User, Category } = require('../models');
+ 
  const editBlogPostService = require('../services/editBlogPostService');
 
- const createPostCategory = async (postId, categoryIds) => {
-  const post = await BlogPost.findByPk(postId);
-
- await categoryIds.forEach(async (categoryId) => {
-    const categories = await Category.findByPk(categoryId);
-    await post.addCategories(categories);
-  });
-};
+ const blogPostService = require('../services/blogPostService');
 
  const findUserEmail = (req) => {
   const token = req.headers.authorization;
@@ -28,42 +22,16 @@
   return userId;
  };
 
- const validateCategories = async (categoryIds, res) => {
-  try {
-        const categories = await Category.findAll({ where: { id: categoryIds } });
-    if (categories.length !== categoryIds.length) {
-      return res.status(400).json({ message: '"categoryIds" not found' }); 
-}
-      return true;
-  } catch (error) {
-    return res.status(400).json({ message: 'error' });
-  }
-    };
-
-    const validatePost = (title, content, categoryIds, res) => {
-      if (!title) { return res.status(400).json({ message: '"title" is required' }); }
-      if (!content) return res.status(400).json({ message: '"content" is required' });
-      if (!categoryIds) {
-      return res.status(400).json({
-          message: '"categoryIds" is required',
-        }); 
-      }
-      return true;
-    };
-  
-  const createBlogPost = async (req, res) => {
+   const createBlogPost = async (req, res) => {
     try {
       const { title, content, categoryIds } = req.body;
-      const validate = validatePost(title, content, categoryIds, res);
-      // await
-      const isValidCategories = (validateCategories(categoryIds, res));
+      const validate = blogPostService.validatePost(title, content, categoryIds, res);
+      const isValidCategories = await (blogPostService.validateCategories(categoryIds, res));
         if (isValidCategories === true);
         if (!validate) return false;
-        // await
-      const userId = findUserId(req);      
+      const userId = await findUserId(req);      
       const { id } = await BlogPost.create({ title, content, userId });
-      // await
-      createPostCategory(id, categoryIds);      
+     await blogPostService.createPostCategory(id, categoryIds);      
       return res.status(201).json({ id, title, content, userId });
     } catch (error) {
       return res.status(500).json(error.message);
@@ -78,7 +46,7 @@
   });   
     return res.status(200).json(getAllUsers);
   } catch (error) {
-    res.status(400).send({ message: 'Something is wrong!!' });
+    res.status(400).send({ message: error.message });
   }
 };
 
@@ -121,30 +89,30 @@ const editBlogPost = async (req, res) => {
   }
 };
 
-const deleteBlogPost = async (req, res) => {
-  const { id } = req.params; 
-  const postId = await BlogPost.findByPk(id);
-  const userId = await findUserId(req);
-  console.log('id', id);
-  console.log('postId', postId.id);
-  if (postId.id === null || id === null) {
-    return res.status(404).json({
-      message: 'Post does not exist',
-    });
-  }
-  console.log('userId', userId);
- if (postId.id !== userId) {
-     return res.status(401).json({ message: 'Unauthorized user' }); 
- }
+// const deleteBlogPost = async (req, res) => {
+//   const { id } = req.params; 
+//   const postId = await BlogPost.findByPk(id);
+//   const userId = await findUserId(req);
+//   console.log('id', id);
+//   console.log('postId', postId.id);
+//   if (postId.id === null || id === null) {
+//     return res.status(404).json({
+//       message: 'Post does not exist',
+//     });
+//   }
+//   console.log('userId', userId);
+//  if (postId.id !== userId) {
+//      return res.status(401).json({ message: 'Unauthorized user' }); 
+//  }
 
-  const deletedPost = await BlogPost.destroy({ where: { id } });
-  return res.status(204).json({ deletedPost });
-};
+//   const deletedPost = await BlogPost.destroy({ where: { id } });
+//   return res.status(204).json({ deletedPost });
+// };
 
 module.exports = {
     createBlogPost,
     getAllBlogPost,
     getPostById,
     editBlogPost,
-    deleteBlogPost,
+    // deleteBlogPost,
   };
