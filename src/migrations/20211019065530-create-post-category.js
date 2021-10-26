8ebd2f4c988cf3b1d3b60d70714f32563caf5104
Gildeir <gildeirlopes@gmail.com>
2021-10-25 22:44:07 -0300

    mudando down da migration para PostsCategories

--- src/migrations/20211019065530-create-post-category.js
+++ src/migrations/20211019065530-create-post-category.js
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('postCategories');
+    await queryInterface.dropTable('PostsCategories');
   }
 };
