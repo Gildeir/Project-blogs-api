2df0b945cce358517858ec57631fc392e0e2b103
Gildeir <gildeirlopes@gmail.com>
2021-10-26 21:57:57 -0300

    alterando postCategorie para postscateogies

--- src/models/postscategory.js
+++ src/models/postscategory.js
@@ -1,21 +1,21 @@
-const PostCategory = (sequelize, _DataTypes) => {
-  const postCategory = sequelize.define('PostCategory', 
+const PostsCategory = (sequelize, _DataTypes) => {
+  const postsCategory = sequelize.define('PostsCategory', 
   {}, { timestamps: false, tableName: 'PostsCategories' });
-  postCategory.associate = (models) => {
+  postsCategory.associate = (models) => {
     models.BlogPost.belongsToMany(models.Category, { 
       as: 'categories',
-      through: postCategory,
+      through: postsCategory,
       foreignKey: 'postId',
       otherKey: 'categoryId',
     });
     models.Category.belongsToMany(models.BlogPost, { 
       as: 'blogPosts',
-      through: postCategory,
+      through: postsCategory,
       foreignKey: 'categoryId',
       otherKey: 'postId',
     });
   };
-  return postCategory;
+  return postsCategory;
 };
 
-module.exports = PostCategory;
+module.exports = PostsCategory;
